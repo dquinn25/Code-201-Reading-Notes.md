640072b1772c40626bedeff690e30bd83e9b8693
David Quinn <david.c.quinn25@gmail.com>
2020-06-04 17:49:58 -0700

    fixed casing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Table of Contents
 
 - [day 1](day-1.md)
-- [day 2](Day-2.md)
+- [day 2](day-2.md)
 - [day 3](day-3.md)
---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+car counter = 1;
+
+function sellVitamins(){
+  var greeting = "howdy my name is david im here to tell you about nutrilite"
+  var extra = 'I will sellyou 3 vitamins for 4 dollars'
+  var thereMore = 'since this is my ' + counter+ 'asking you';
+}
+
+
+function petGinger () {
+  console.log('I petted ginger');
+}
+
+var petSnowdrop = function potatoeSnowDrop()
+console.log('I petted da snowdrop, ouch she kneaded me')
+);
+
+petSnowdrop();
+
+document.getElementById('someButton').addEventListener('click', function (){
+  console.log('clicked');
+})
+
+function countPets(numberOfPets, typeOfPets){
+  console.log('you have' + numberOfPets + '' + typeOfPets);
+}
+
